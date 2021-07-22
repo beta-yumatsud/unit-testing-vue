97830627a64105e9ded9b8acad2c1c15efbe87dd
Beta Yumatsud <hcksx908@yahoo.co.jp>
2021-07-22 17:04:01 +0900

    一通りやったった

---
+++ tests/unit/LoginForm.spec.js
@@ -0,0 +1,17 @@
+import { mount } from "@vue/test-utils"
+import LoginForm from "@/components/LoginForm"
+
+describe('LoginForm', () => {
+  it('emits an event with a user data payload', () => {
+    const wrapper = mount(LoginForm)
+    const input = wrapper.find('input[type="text"]')
+    input.setValue('Adam Jahr')
+    wrapper.trigger('submit')
+    
+    const formSubmittedCalls = wrapper.emitted('formSubmitted')
+    expect(formSubmittedCalls).toHaveLength(1)
+
+    const expectedPayload = { name: 'Adam Jahr' }
+    expect(wrapper.emitted('formSubmitted')[0][0]).toMatchObject(expectedPayload)
+  })
+})
